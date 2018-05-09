f30c51181fb31727242008e31c84f331dba11c22
Steevan BARBOYON <steevan.barboyon@gmail.com>
2018-05-09 22:09:55 +0200

    Create Hello world benchmark

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+$varDir = __DIR__ . '/../var';
+require_once '../vendor/phpbenchmarks/smarty-common/helloworld.php';
+
+// require phpbenchmarks stats.php here when needed
